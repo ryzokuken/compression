44ae4bb9adc67a62b9d4e260558c8c46d3e6dd33
Ujjwal Sharma <usharma1998@gmail.com>
2019-08-10 11:37:31 +0530

    Turn binary into library

---
+++ src/lib.rs
@@ -0,0 +1,2 @@
+pub mod huffman;
+pub mod tree;
--- src/main.rs
+++
@@ -1,9 +0,0 @@
-mod huffman;
-mod tree;
-
-fn main() {
-    let string = "AAAABBBCCD";
-    let compressed = huffman::huffman(string);
-    println!("{:?}", compressed.into_boxed_slice());
-    println!("{:?}", string.as_bytes());
-}
